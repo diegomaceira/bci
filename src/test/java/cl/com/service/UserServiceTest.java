1da41b208a785d137919540b35b9b51bec1adc1c
dmaceira <maceira.diego@gmail.com>
2022-08-22 15:38:19 -0300

    test

--- src/test/java/cl/com/service/UserServiceTest.java
+++ src/test/java/cl/com/service/UserServiceTest.java
@@ -1,22 +1,42 @@
 package cl.com.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import cl.com.model.User;
 
-class UserServiceTest{
+//public class UserServiceTest extends DBTestCase{
+		class UserServiceTest{
 
 	UserService userService=mock(UserService.class);
 	
-	//UserRepository userRepository=mock(UserRepository.class);
-	
+	//UserRepository userRepository=mock(UserRepository.class);	
+
+	/*
+    public UserServiceTest(String name)
+    {
+        super( name );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.h2.Driver" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:h2:mem:dcbapp" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "sa" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "password" );
+	// System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "" );
+    }
+    
+    protected IDataSet getDataSet() throws Exception
+    {
+       // return new FlatXmlDataSetBuilder().build(new FileInputStream("dataset.xml"));
+    	return null;
+    }
+	*/
 	@BeforeEach
 	public void beforeClass() {
 	}
@@ -27,7 +47,7 @@ class UserServiceTest{
 		//fail("Not yet implemented");
 
 		//System.out.println("getall:" + userService.getAllUser());
-		assertEquals("c", "c");
+		//assertEquals("c", "c");
 	}
 
 	@Test
@@ -46,7 +66,7 @@ class UserServiceTest{
 		//when(userService.save(user)).thenReturn((ResponseEntity<?>) ResponseEntity.ok(HttpStatus.OK));
 		
 		//System.out.println("saved:" + userService.save(user));
-		assertEquals("c", "c");
+		//assertEquals("c", "c");
 	}
 
 }
